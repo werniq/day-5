0e2e9052f918f13c3b3f1ded12f5e8999e0332e0
werniq <qniwwwersss@gmail.com>
2022-12-15 10:04:46 +0100

    asd

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"myapp/internal/models"
+	"net/http"
+)
 
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
@@ -18,8 +21,8 @@ func (app *application) succededPayment(w http.ResponseWriter, r *http.Request) 
 	}
 
 	// read posted data 
-	cardHolder := r.Form.Get("cardholder-name")
-	email := r.Form.Get("email")
+	cardHolder := r.Form.Get("cardholder_name")
+	email := r.Form.Get("cardholder_email")
 	paymentIntent := r.Form.Get("payment_intent")
 	paymentMethod := r.Form.Get("payment_method")
 	paymentAmount := r.Form.Get("payment_amount")
@@ -34,10 +37,38 @@ func (app *application) succededPayment(w http.ResponseWriter, r *http.Request) 
 	data["payment_amount"] = paymentAmount
 	data["Currency"] = Currency
 
+	// should write this data tp session, and then redirect user to new page
 
 	if err := app.renderTemplate(w, r, "succeeded", &templateData{
 		Data: data,
 	}); err != nil {
+		app.errorLog.Println(err)
+	}
+}
+
+
+// ChargeOnce displays the page to buy one widget
+func (app *application) ChargeOnce(w http.ResponseWriter, r *http.Request) {
+	
+	Widget := models.Widget{
+		Id: 1,
+		Name: "Custom widget",
+		Description: "Very nice widget",
+		InventoryLevel: 10,
+		Price: 1000,
+	}
+	data := make(map[string]interface{})
+	data["widget"] = Widget
+	if err := app.renderTemplate(w, r, "buy-once", &templateData{
+		Data: data,
+	}); err != nil {
+		app.errorLog.Println(err)
+	}
+}
+
+
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
+	if err := app.renderTemplate(w, r, "home", nil); err != nil {
 		app.errorLog.Println(err)
 	}
 }
