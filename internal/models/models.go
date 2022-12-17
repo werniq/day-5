a5c44b1c7b2053e07168add12e75bab8d81e0958
werniq <qniwwwersss@gmail.com>
2022-12-17 09:43:46 +0100

    New models created

--- internal/models/models.go
+++ internal/models/models.go
@@ -11,7 +11,6 @@ type DBModel struct {
 	DB *sql.DB
 }
 
-
 // Models is the wrapper for all models
 type Models struct {
 	DB DBModel
@@ -24,28 +23,152 @@ func NewModels(db *sql.DB) Models {
 	}
 }
 
-// 
 type Widget struct {
-	Id 				int `json:"id"`
-	Name 			string `json:"name"`
-	Description 	string `json:"description"`
-	InventoryLevel  int `json:"inventory_level"`
-	Price 			int `json:"price"`
-	CreatedAt 		time.Time `json:"-"`
-	UpdatedAt 		time.Time `json:"-"`
+	Id             int       `json:"id"`
+	Name           string    `json:"name"`
+	Description    string    `json:"description"`
+	InventoryLevel int       `json:"inventory_level"`
+	Image          string    `json:"image"`
+	Price          int       `json:"price"`
+	CreatedAt      time.Time `json:"-"`
+	UpdatedAt      time.Time `json:"-"`
 }
 
+type Order struct {
+	ID        int       `json:"id"`
+	WidgetID  int       `json:"widgetId"`
+	TxId      int       `json:"transactionID"`
+	StatusID  int       `json:"statusID"`
+	Quantity  int       `json:"quantity"`
+	Amount    int       `json:"amount"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
+
+type Statuses struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
+
+type TxStatuses struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
+
+type Tx struct {
+	ID                  int       `json:"id"`
+	Amount              int       `json:"amount"`
+	Currency            string    `json:"currency"`
+	LastFour            int       `json:"last_four"`
+	BankReturnCode      string    `json:"bankReturnCode"`
+	TransactionStatusID int       `json:"txstatusid"`
+	Name                string    `json:"name"`
+	CreatedAt           time.Time `json:"-"`
+	UpdatedAt           time.Time `json:"-"`
+}
+
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
 
 func (m *DBModel) GetWidget(id int) (Widget, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second) 
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	var widget Widget
 
-	row := m.DB.QueryRowContext(ctx, "select id, name from Widgets where id = ?", id)
-	err := row.Scan(&widget.Id, &widget.Name)
+	row := m.DB.QueryRowContext(ctx, `
+			select 
+				id, name, description, inventory_level, price, coalesce(, ''), 
+				created_at, updated_at 
+			from 
+				Widgets
+			where id = ?`, id)
+
+	err := row.Scan(
+		&widget.Id,
+		&widget.Name,
+		&widget.Description,
+		&widget.InventoryLevel,
+		&widget.Price,
+		&widget.Image,
+		&widget.CreatedAt,
+		&widget.UpdatedAt,
+	)
+
 	if err != nil {
 		return widget, err
 	}
+
 	return widget, nil
+}
+
+// save transaction function
+// Inserts a new txn and returns its id
+func (m *DBModel) InsertTransaction(txn Tx) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `insert into transactions (amount, currency, last_four, bankReturnCode, txsstatusid, created_at, updated_at
+		values(?, ?, ?, ?, ?, ?, ?)`
+
+	res, err := m.DB.ExecContext(ctx, stmt,
+		txn.Amount,
+		txn.Currency,
+		txn.LastFour,
+		txn.BankReturnCode,
+		txn.TransactionStatusID,
+		time.Now(),
+		time.Now(),
+	)
+
+	if err != nil {
+		return 0, err
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, nil
+	}
+
+	return int(id), nil
+}
+
+func (m DBModel) InsertOrder(order Order) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `insert into 
+		orders
+			(widgetId, txId, statusId, quantity, created_at, updated_at)
+		values
+			(?, ?, ?, ?, ?, ?, ?)`
+	res, err := m.DB.ExecContext(ctx, stmt,
+		order.WidgetID,
+		order.TxId,
+		order.StatusID,
+		order.Quantity,
+		order.Amount,
+		time.Now(),
+		time.Now(),
+	)
+
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
